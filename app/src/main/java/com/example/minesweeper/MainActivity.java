44902c5d6ad84259ce8f8e1aed562d5e5e35a22f
tqtrung02 <tqtrung02@gmail.com>
2023-06-17 12:23:42 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.minesweeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.drawable.GradientDrawable;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements OnCellClickListener {
+    public static final long TIMER_LENGTH = 999000L;
+    public static final int BOMB_COUNT = 10;
+    public static final int GRID_SIZE = 10;
+    private RecyclerView grid;
+    private MineGridRecyclerAdapter mineGridRecyclerAdapter;
+    private MinesweeperGame mineSweeperGame;
+    private TextView smiley, timer, flagsLeft, flag;
+    private CountDownTimer countDownTimer;
+    private int secondsElapsed;
+    private boolean timerStarted;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        grid = findViewById(R.id.activity_main_grid);
+        grid.setLayoutManager(new GridLayoutManager(this, 10));
+
+        timer = findViewById(R.id.activity_main_timer);
+        timerStarted = false;
+        countDownTimer = new CountDownTimer(TIMER_LENGTH, 1000) {
+            public void onTick(long millisUntilFinished) {
+                secondsElapsed += 1;
+                timer.setText(String.format("%03d", secondsElapsed));
+            }
+
+            public void onFinish() {
+                mineSweeperGame.outOfTime();
+                Toast.makeText(getApplicationContext(), "Game Over: Timer Expired", Toast.LENGTH_SHORT).show();
+                mineSweeperGame.getMineGrid().revealAllBombs();
+                mineGridRecyclerAdapter.setCells(mineSweeperGame.getMineGrid().getCells());
+            }
+        };
+
+        flagsLeft = findViewById(R.id.activity_main_flagsleft);
+
+        mineSweeperGame = new MinesweeperGame(GRID_SIZE, BOMB_COUNT);
+        flagsLeft.setText(String.format("%03d", mineSweeperGame.getNumberBombs() - mineSweeperGame.getFlagCount()));
+        mineGridRecyclerAdapter = new MineGridRecyclerAdapter(mineSweeperGame.getMineGrid().getCells(), this);
+        grid.setAdapter(mineGridRecyclerAdapter);
+
+        smiley = findViewById(R.id.activity_main_smiley);
+        smiley.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mineSweeperGame = new MinesweeperGame(GRID_SIZE, BOMB_COUNT);
+                mineGridRecyclerAdapter.setCells(mineSweeperGame.getMineGrid().getCells());
+                timerStarted = false;
+                countDownTimer.cancel();
+                secondsElapsed = 0;
+                timer.setText(R.string.default_count);
+                flagsLeft.setText(String.format("%03d", mineSweeperGame.getNumberBombs() - mineSweeperGame.getFlagCount()));
+            }
+        });
+
+        flag = findViewById(R.id.activity_main_flag);
+        flag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mineSweeperGame.toggleMode();
+                if (mineSweeperGame.isFlagMode()) {
+                    GradientDrawable border = new GradientDrawable();
+                    border.setColor(0xFFFFFFFF);
+                    border.setStroke(1, 0xFF000000);
+                    flag.setBackground(border);
+                } else {
+                    GradientDrawable border = new GradientDrawable();
+                    border.setColor(0xFFFFFFFF);
+                    flag.setBackground(border);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void cellClick(Cell cell) {
+        mineSweeperGame.handleCellClick(cell);
+
+        flagsLeft.setText(String.format("%03d", mineSweeperGame.getNumberBombs() - mineSweeperGame.getFlagCount()));
+
+        if (!timerStarted) {
+            countDownTimer.start();
+            timerStarted = true;
+        }
+
+        if (mineSweeperGame.isGameOver()) {
+            countDownTimer.cancel();
+            Toast.makeText(getApplicationContext(), "Game Over", Toast.LENGTH_SHORT).show();
+            mineSweeperGame.getMineGrid().revealAllBombs();
+            MediaPlayer music = MediaPlayer.create(MainActivity.this, R.raw.explosion);
+            music.start();
+        }
+
+        if (mineSweeperGame.isGameWon()) {
+            countDownTimer.cancel();
+            Toast.makeText(getApplicationContext(), "Game Won", Toast.LENGTH_SHORT).show();
+            mineSweeperGame.getMineGrid().revealAllBombs();
+            MediaPlayer music = MediaPlayer.create(MainActivity.this, R.raw.win);
+            music.start();
+        }
+
+        mineGridRecyclerAdapter.setCells(mineSweeperGame.getMineGrid().getCells());
+    }
+}
---
+++ app/src/main/java/com/example/minesweeper/OnCellClickListener.java
@@ -0,0 +1,5 @@
+package com.example.minesweeper;
+
+public interface OnCellClickListener {
+    void cellClick(Cell cell);
+}
